1d3b284ae2e7421b666b5e2c8db639b59fd5b0e1
Hillel Familant <hillel.familant@macys.com>
2020-02-21 16:17:36 -0800

    Fixing vulnerabilities in modules

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import JokeGenerator from './jokeGenerator';
-import PoemGenerator from './poemGenerator';
+import "./App.css";
+import JokeGenerator from "./jokeGenerator";
+import PoemGenerator from "./poemGenerator";
 
 function App() {
   return (
     <div className="App">
       <h1>Poems, Jokes and Webcams.</h1>
       <div>
-			  <PoemGenerator />
-			</div>
+        <PoemGenerator />
+      </div>
       <div>
         <JokeGenerator />
       </div>
-      <div></div>
+      <div>{/* <QuoteGenerator /> */}</div>
     </div>
   );
 }
